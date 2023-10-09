2a2270687533ae7f08027f116be86a40f76dd4b4
Pollikkk <107186617+Pollikkk@users.noreply.github.com>
2023-10-09 17:07:05 +0300

    Add files via upload

---
+++ Лабораторная 1/task2.py
@@ -0,0 +1,12 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+kol_players = len(list_players)
+kol_team = kol_players//2
+
+players_1 = list_players[:kol_team]
+players_2 = list_players[kol_team:]
+
+# TODO Разделите участников на две команды
+
+print(players_1)
+print(players_2)
